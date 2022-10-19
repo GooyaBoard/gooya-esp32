3fcc69c35c04397a67b39278dcd907d34508c8a3
Hisaharu SUZUKI <h.suzuki@evixar.com>
2022-10-19 20:53:30 +0900

    modify i2s config for newest esp-idf sdk

--- I2SMaster.cpp
+++ I2SMaster.cpp
@@ -61,13 +61,14 @@ void I2SMaster::initialize(int sample_rate, int cpu_number,
     i2s_pin_config.ws_io_num = I2S_ADC_LRCLK;
     i2s_pin_config.data_in_num = I2S_ADC_DATA;
     i2s_pin_config.data_out_num = I2S_DAC_DATA;
+    i2s_pin_config.mck_io_num = 0;
 
     // I2S setting
     i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_TX);
     i2s_config.sample_rate = sample_rate;
     i2s_config.bits_per_sample = I2S_BITS_PER_SAMPLE;
     i2s_config.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT;
-    i2s_config.communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB);
+    i2s_config.communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_STAND_I2S);
     i2s_config.intr_alloc_flags = ESP_INTR_FLAG_LEVEL1;
     i2s_config.dma_buf_count = DMA_BUFFER_COUNT;
     i2s_config.dma_buf_len = DMA_AUDIO_FRAMES;
